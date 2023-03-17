ff9237c057f71da904e647ed85793db069129a1b
Thomas Newton <2668168N@student.gla.ac.uk>
2023-03-17 14:08:59 +0000

    Almost finished everything

--- CuisineCritic/CuisineCritic/settings.py
+++ CuisineCritic/CuisineCritic/settings.py
@@ -18,6 +18,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
