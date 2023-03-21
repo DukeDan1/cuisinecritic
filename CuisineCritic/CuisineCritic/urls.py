d22abbd2a54ed0a581cc83a64447e0f39d2b7b54
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2023-03-21 23:35:37 +0000

    Implement database queries for restaurant view

--- CuisineCritic/CuisineCritic/urls.py
+++ CuisineCritic/CuisineCritic/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import views
+import main.views as views
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,21 +29,32 @@ urlpatterns = [
     path('restaurants', views.restaurants, name='restaurants'),
     path('profile', views.profile, name='profile'),
     path('account', views.account, name='account'),
-    path('chineseManorHouse', views.chineseManorHouse, name='chineseManorHouse'),
-    path('shanghaiShuffle', views.shanghaiShuffle, name='shanghaiShuffle'),
-    path('luckyStar', views.luckyStar, name='luckyStar'),
-    path('alessi', views.alessi, name='alessi'),
-    path('bombayBanquet', views.bombayBanquet, name='bombayBanquet'),
-    path('easternPavilion', views.easternPavilion, name='easternPavilion'),
-    path('topolobamba', views.topolobamba, name='topolobamba'),
-    path('elPastor', views.elPastor, name='elPastor'),
-    path('wahaca', views.wahaca, name='wahaca'),
-    path('phucket', views.phucket, name='phucket'),
-    path('kaoSarn', views.kaoSarn, name='kaoSarn'),
-    path('theMangoTree', views.theMangoTree, name='theMangoTree'),
     path('forgottenPassword', views.forgottenPassword, name='forgottenPassword'),
-    path('login/forgottenPassword.html', views.forgottenPassword, name='forgottenPassword'),
+     path('login/forgottenPassword.html', views.forgottenPassword, name='forgottenPassword'),
+
+
+    # Replaced by automated restaurant pages:
+    # path('chineseManorHouse', views.chineseManorHouse, name='chineseManorHouse'),
+    # path('shanghaiShuffle', views.shanghaiShuffle, name='shanghaiShuffle'),
+    # path('luckyStar', views.luckyStar, name='luckyStar'),
+    # path('alessi', views.alessi, name='alessi'),
+    # path('bombayBanquet', views.bombayBanquet, name='bombayBanquet'),
+    # path('easternPavilion', views.easternPavilion, name='easternPavilion'),
+    # path('topolobamba', views.topolobamba, name='topolobamba'),
+    # path('elPastor', views.elPastor, name='elPastor'),
+    # path('wahaca', views.wahaca, name='wahaca'),
+    # path('phucket', views.phucket, name='phucket'),
+    # path('kaoSarn', views.kaoSarn, name='kaoSarn'),
+    # path('theMangoTree', views.theMangoTree, name='theMangoTree'),
+
+
+    # Render restaurant
+    path('restaurants/<slug:restaurant_id>', views.render_restaurant, name='show_restaurant'),
     
     # API URL endpoints:
     path('api/login', views.api_login, name='api_login'),
-]
+    path('api/register', views.api_register, name='api_register'),
+    path('api/search', views.api_search, name='api_search'),
+    path('api/createRestaurant', views.api_create_restaurant, name='api_create_restaurant'),
+    path('api/submitReview', views.api_submit_review, name='api_submit_review'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
