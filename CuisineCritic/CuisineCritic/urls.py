aea8ca74c6a2791fd1acc3d185baad1c8e7eccbb
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2023-03-23 21:05:29 +0000

    Add image uploading functionality for new users

--- CuisineCritic/CuisineCritic/urls.py
+++ CuisineCritic/CuisineCritic/urls.py
@@ -57,11 +57,16 @@ urlpatterns = [
     path('category', views.redirect_restaurant, name='category_list'),
     path('category/', views.redirect_restaurant, name='category_list'),
     path('category/<slug:category_slug>', views.render_category, name='show_category'),
+
+    # Add restaurant form
+    path('add-restaurant', views.add_restaurant, name='add_restaurant'),
     
     # API URL endpoints:
     path('api/login', views.api_login, name='api_login'),
     path('api/register', views.api_register, name='api_register'),
     path('api/search', views.api_search, name='api_search'),
-    path('api/createRestaurant', views.api_create_restaurant, name='api_create_restaurant'),
+    path('api/create_restaurant', views.api_create_restaurant, name='api_create_restaurant'),
     path('api/review', views.api_submit_review, name='api_submit_review'),
+    path('api/upload_avatar', views.api_upload_avatar, name='api_upload_avatar'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
