f677bb19012aa1e24aef69902064395a88df03f1
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2023-03-24 01:58:01 +0000

    try to fix Python Anywhere

--- CuisineCritic/main/apps.py
+++ CuisineCritic/main/apps.py
@@ -1,6 +1,8 @@
-from django.apps import AppConfig    
+from django.apps import AppConfig
+from CuisineCritic.CuisineCritic.settings import BASE_DIR    
 
 class CuisineCriticConfig(AppConfig):
     name = 'CuisineCritic'
     verbose_name = 'Cuisine Critic'
     label = "Cuisine Critic"
+    path = BASE_DIR+"/main"
--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 while true; do
-    if [ $1 == "pythonanywhere" ]; then
-        echo "Starting pythonanywhere server..."
-        python3 CuisineCritic/manage.py runserver 0.0.0.0:8000
-    else
-        echo "Starting local server..."
-        python3 CuisineCritic/manage.py runserver 12000
-    fi
-    echo "Server exited with code $?. Respawning.." >&2
+    echo "Starting server..."
+    python3 CuisineCritic/manage.py runserver 12000
+    echo "Server exited. Respawning.."
     sleep 5
 done
