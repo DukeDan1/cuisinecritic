9d5b7d1c7b9c7eb7f21f9af108c3810923fd4a1d
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2023-03-24 14:09:42 +0000

    remove path declaration, change readme

--- CuisineCritic/main/apps.py
+++ CuisineCritic/main/apps.py
@@ -6,4 +6,4 @@ class CuisineCriticConfig(AppConfig):
     name = 'CuisineCritic'
     verbose_name = 'Cuisine Critic'
     label = "Cuisine Critic"
-    path = os.path.join(settings.BASE_DIR, 'main')
+    #path = os.path.join(settings.BASE_DIR, 'main')
--- cleandb.sh
+++ cleandb.sh
@@ -1,5 +1,8 @@
-rm CuisineCritic/db.sqlite3
+if [ -f CuisineCritic/db.sqlite3 ]; then
+    rm CuisineCritic/db.sqlite3
+fi
+
 python CuisineCritic/manage.py makemigrations
 python CuisineCritic/manage.py migrate
 python CuisineCritic/manage.py migrate --run-syncdb
-python CuisineCritic/populate.py
+python CuisineCritic/population_script.py
